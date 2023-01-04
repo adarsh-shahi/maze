9747c55cdca2538bf2da05cfd679ffe3e0bc8c58
adarsh-shahi <adarshshahi1009@gmail.com>
2023-01-04 15:51:19 +0530

    randomly ordered neighbours of cells

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Engine, Render, Runner, World, Bodies } = Matter;
 const engine = Engine.create();
 const { world } = engine;
 
+const cells = 3; // 3 * 3 (horizontal = 3 & vertical = 3) = 9
 const width = 600;
 const height = 600;
 
@@ -43,3 +44,66 @@ const walls = [
 ];
 
 World.add(world, walls);
+
+// * Maze Generation
+
+const shuffle = (arr) => {
+	let counter = arr.length;
+
+	while (counter > 0) {
+		const index = Math.floor(Math.random() * counter);
+		counter--;
+
+		const temp = arr[counter];
+		arr[counter] = arr[index];
+		arr[index] = temp;
+	}
+  return arr
+};
+
+let grid = Array(cells)
+	.fill(null)
+	.map(() => {
+		return Array(cells).fill(false);
+	});
+
+// for (let i = 0; i < 3; i++) {
+// 	grid.push([]);
+// 	for (let j = 0; j < 3; j++) {
+// 		grid[i].push(false);
+// 	}
+// }
+
+let verticals = Array(cells)
+	.fill(null)
+	.map(() => {
+		return Array(cells - 1).fill(false);
+	});
+
+let horizontals = Array(cells - 1)
+	.fill(null)
+	.map(() => {
+		return Array(cells).fill(false);
+	});
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepTroughCell = (row, column) => {
+	// if already visited the cell at [row, column], then return
+	if (grid[row][column]) return; // already visited return
+
+	// mark cell as visited
+	grid[row][column] = true;
+
+	// assemble randomly-ordered list of neighbours
+	const neighbours = shuffle([
+		[row - 1, column],
+		[row, column + 1],
+		[row + 1, column],
+		[row, column - 1],
+	]);
+
+};
+
+stepTroughCell(startRow, startColumn);
